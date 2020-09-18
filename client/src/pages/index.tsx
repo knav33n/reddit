82c6a14a337848d3e079b1b7b923f9b4798de5aa
naveen <nnaveen.kumar@live.com>
2020-09-18 10:20:34 +0530

    Refactor urql setup to a HOC
    Add posts query

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,20 @@
+import { withUrqlClient } from "next-urql";
 import NavBar from "../components/NavBar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>hello</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      {!data ? (
+        <div>Loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
